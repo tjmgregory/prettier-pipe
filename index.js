31e2ebeddf16dfe252535dac2ac7d80e2c34f67d
Theo Gregory <tjmg@outlook.com>
2022-06-07 18:48:20 +0100

    feat: adds working basic pipe

---
+++ index.js
@@ -0,0 +1,6 @@
+const prettier = require("prettier");
+const fs = require("fs");
+
+const data = fs.readFileSync(process.stdin.fd, "utf-8");
+
+console.log(prettier.format(data, { semi: false, parser: "babel" }));
