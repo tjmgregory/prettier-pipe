6bfb9194bf3faf8ec22f3504cf6f3fdbe6e17803
Theo Gregory <tjmg@outlook.com>
2022-12-14 10:58:08 +0000

    feat!: defaults to tsx instead of babel when no filepath is given

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const fs = require("fs");
 const data = fs.readFileSync(process.stdin.fd, "utf-8");
 
 // Grab the filename that was ideally provided, which will allow pretter to
-// infer the correct parser
+// infer the correct parser. Default to tsx as that at least covers js,jsx,ts,tsx
 const filepath = process.argv[2];
-const prettierConfigDefault = filepath ? { filepath } : { parser: "babel" };
+const prettierConfigDefault = filepath ? { filepath } : { filepath: 'foo.tsx' };
 
 // Find your nearest prettier config
 prettier.resolveConfig(process.cwd()).then((config) => {
